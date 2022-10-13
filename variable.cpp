7838ffd787c5d9b7d6e6cc1e934af9bf486dc170
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-10-14 00:07:14 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    long l;
+    char ch;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf",&i,&l,&ch,&f,&d);
+    //printf("%d %ld %c %f %lf",i,"\n",l,"\n",ch,"\n",f,"\n",d);
+    cout<< i <<"\n"<<l<<"\n"<<ch<<"\n"<<f<<"\n"<<d;
+    return 0;
+
+
+}
