d783c2ef1bcc8c33ee42ad77e932bd28aac7e95c
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-10-13 23:47:31 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,33 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
     public static void main(String[] args) {
-        int arr[][] = { { 1, 1, 0, 0, 0 }, { 1, 1, 1, 1, 0 }, { 1, 0, 0, 0, 0 }, { 1, 1, 0, 0, 0 }, { 1, 1, 1, 1, 1 } };
-        Map<Integer, Integer> res = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            int count = 0;
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] == 1) {
-                    count++;
-                }
+        int arr[] = { 6, 2, 9, 1, 7 };
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
             }
-            res.put(i, count);
         }
-        for (int i = 0; i < res.size(); i++) {
-            int min = res.get(0);
-            int index = 0;
-            for (int j = 0; j < res.size(); j++) {
-                if (res.get(j) != -1) {
-                    if (res.get(j) < min) {
-                        min = res.get(j);
-                        index = j;
-                    }
-                }
-                else{ index++;}
-            }
-            System.out.println(index);
-            res.put(index, -1);
-        }
+        System.out.println(largest);
     }
 }
